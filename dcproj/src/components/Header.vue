5092effd1f15669fa13762cc220fc7c6a034f395
Gianluca <gl.galassini@gmail.com>
2021-07-07 15:12:20 +0200

    navbar dinamica con v-for

--- dcproj/src/components/Header.vue
+++ dcproj/src/components/Header.vue
@@ -6,9 +6,7 @@
 
       <nav>
         <ul>
-          <li><a href="#">Ciccio</a></li>
-          <li><a href="#">Bello</a></li>
-          <li><a href="#">Pippo</a></li>
+          <li v-for="(item, i) in navItems " :key="i"><a :href="item.url">{{item.text}}</a></li>
         </ul>
       </nav>
 
@@ -19,6 +17,52 @@
 <script>
 export default {
   name: 'Header',
+  data() {
+    return {
+      navItems: [
+        {
+          text:"characters",
+          url:"#"
+        },
+        {
+          text:"comics",
+          url:"#"
+        },
+        {
+          text:"movies",
+          url:"#"
+        },
+        {
+          text:"tv",
+          url:"#"
+        },
+        {
+          text:"games",
+          url:"#"
+        },
+        {
+          text:"collectibles",
+          url:"#"
+        },
+        {
+          text:"videos",
+          url:"#"
+        },
+        {
+          text:"fans",
+          url:"#"
+        },
+        {
+          text:"news",
+          url:"#"
+        },
+        {
+          text:"shop",
+          url:"#"
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -38,6 +82,7 @@ img {
 ul {
   list-style: none;
   font-weight: 700;
+  text-transform: uppercase;
 
   li {
   display: inline-block;
