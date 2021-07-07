2365b015e98221f01fe28ba44fee619b1e57b218
Gianluca <gl.galassini@gmail.com>
2021-07-07 15:59:48 +0200

    add main vuoto

--- dcproj/src/App.vue
+++ dcproj/src/App.vue
@@ -5,26 +5,26 @@
       <Header/>
     </header>
 
-    <!-- <main>
-      <Main>
+    <main>
+      <Main/>
     </main>
 
-    <footer>
-      <Footer>
+    <!-- <footer>
+      <Footer/>
     </footer> -->
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-// import Main from './components/Main.vue'
+import Main from './components/Main.vue'
 // import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    // Main,
+    Main,
     // Footer
   }
 }
---
+++ dcproj/src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div class="container">
+          <p> --&gt; insert content here &lt;--</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+    height: 80px;
+    background-color: black;
+}
+
+p {
+    color:#fff;
+    font-weight: 700;
+    font-size:3rem;
+    text-align: center;
+    line-height: 80px;
+}
+
+</style>
