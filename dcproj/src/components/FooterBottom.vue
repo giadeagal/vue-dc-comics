a127cc18b7aa930d525fa6fe440f9774a7deba4a
Gianluca <gl.galassini@gmail.com>
2021-07-08 01:10:43 +0200

    struttura footerbottom

--- dcproj/src/components/FooterBottom.vue
+++ dcproj/src/components/FooterBottom.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-      <p>FooterBottom</p>
+     <div class="container">
+       <div class="button"></div>
+       <div class="socials"></div>
+     </div>
   </div>
 </template>
 
@@ -11,6 +14,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+@import "../style/vars.scss";
+
+.container {
+  height: 80px;
+  width: 100%;
+  background-color: $notgray;
+  display: flex;
+  justify-content: space-around;
+
+}
+
+.button,
+.socials {
+  max-height: 50px;
+  min-width: 50px;
+  background-color: teal;
+  transform: translateY(25%);
+}
 
 </style>
